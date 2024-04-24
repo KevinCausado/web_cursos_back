182746f3c9c40d746e9f978eea5f93d9335b9359
KevinCausado <kevincausado@gmail.com>
2024-04-24 09:40:23 -0500

    Update and find courses by id

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -10,10 +10,28 @@ module.exports = {
     }
   },
 
+  async getCourse(req, res) {
+    try {
+      const result = await Course.findById(req.params.id);
+      res.status(200).json({data:result});
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
   async saveCourses(req, res) {
     try {
       const result = await Course.create(req.body);
       res.status(201).json({message:'Registro creado exitosamente',data:result});
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  async UpdateCourse(req, res) {
+    try {
+      const result = await Course.findByIdAndUpdate(req.params.id,req.body,{new:true})
+      res.status(200).json({message:'Registro actualizado exitosamente',data:result});
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
